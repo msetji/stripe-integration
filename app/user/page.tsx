ea0ed56b9188a37f35b26d68b737e42f9e1c6001
Michael Setji <michaelpsetji@gmail.com>
2024-09-25 12:13:27 -0400

    basic integration

---
+++ app/user/page.tsx
@@ -0,0 +1,12 @@
+import CheckoutButton from "./CheckoutButton";
+import LoginForm from "./LoginForm";
+import UserProfile from "./UserProfile";
+
+export default function Login() {
+  return (
+    <main>
+      <h1>Login</h1>
+      <CheckoutButton/>
+    </main>
+  );
+}
